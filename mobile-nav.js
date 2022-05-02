1746a05566b02d69f5794d5b1816cf0404ffcd1f
lkwebdev <lkwebdev@mail.ru>
2022-05-03 01:42:07 +0300

    ~

--- mobile-nav.js
+++ mobile-nav.js
@@ -1,7 +1,22 @@
 $(document).ready(() => {
-    let mobileTemplate = ``;
+    let mobileTemplate = `<div class="mobile-categories">`;
+    let parentCategories = $('.category .parent');
+    parentCategories.each((index, el) => {
+        console.log(el);
+        mobileTemplate += el.outerHTML;
+    });
 
 
-
+    mobileTemplate += `</div>`;
     $('#app').append(mobileTemplate);
+
+
+    $('.parent').click((e) => {
+        e.preventDefault();
+
+        let currentEl = $(e.currentTarget);
+        let categoryId = currentEl.data('category-id');
+
+        $(`.category[data-category-id="${categoryId}"]`);
+    });
 });
